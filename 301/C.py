5b228c5755bd0dff6003f6f589335e45dd27ddbb
SaeYoshizaki <yoshizakisae@gmail.com>
2025-07-24 23:39:17 +0900

    完成

--- 301/C.py
+++ 301/C.py
@@ -1,22 +1,3 @@
-
-
-"""
-自分に含まれておらず(いて)、相手に含まれている(いない)ものが全てatcoderの中で置き換えられればOK
-
-1. それぞれの文字に対して、何が何個含まれているのかを辞書で整理
-2. 比較
-    その値のものが相手に幾つあるのかを比較？
-    SをベースにTを探索していく。
-    アルファベット順に並んでいるはずだから、一致するものが出てくるまで繰り返す？？
-    見つかり次第最初に戻る
-    計算量がやばそう
-
-3. 違う文字の数分＠が含まれていない場合 -> No確定&終了
-4. 足りないもの同士がatcoderの文字で表せるものなのかを判定
-5. できる -> Yes, できない -> No
-
-"""
-
 from collections import Counter
 
 
@@ -37,8 +18,6 @@ def main():
 
     Sdict = Counter(S)
     Tdict = Counter(T)
-    # Sdict_sorted = func(sorted(Sdict.items(), key=lambda x:x[0]))
-    # Tdict_sorted = func(sorted(Tdict.items(), key=lambda x:x[0]))
 
     atcoder = list("atcoder")
 
@@ -46,20 +25,19 @@ def main():
     Scope = set(Sdict.keys()) | set(Tdict.keys()) 
     if "@" in Scope:
         Scope.remove("@")
-    # print(Scope)
 
     for i in Scope:
         if Sdict[i] == Tdict[i]:
             continue
         elif Sdict[i] > Tdict[i]:
-            if i in atcoder:
-                Tdict["@"] = Tdict.get("@", 0) - Sdict.get(i, 0) - Tdict.get(i, 0)
+            if i in atcoder and "@" in Tdict.keys() and Tdict["@"] >= (Sdict[i] - Tdict[i]):
+                Tdict["@"] -= (Sdict[i] - Tdict[i])
             else:
                 print("No")
                 return
         else:
-            if i in atcoder:
-                Sdict["@"] = Sdict.get("@", 0) - Tdict.get(i, 0) - Sdict.get(i, 0)
+            if i in atcoder and "@" in Sdict.keys() and Sdict["@"] >= (Tdict[i] - Sdict[i]):
+                Sdict["@"] -= (Tdict[i] - Sdict[i])
             else:
                 print("No")
                 return
@@ -85,4 +63,7 @@ T: choku@@i
 やっぱり少ない方の＠の数をひくのが一番？？
 ＠は一番最後に大きさが一致すれば十分である。
 S-T 
+atcoder
+atcodert
+at@@@@@@
 """
